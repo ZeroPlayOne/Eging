fb1e3edef4f8c3289125ef34a9ea4161e28ffe20
ZeroPlayOne <154983607+ZeroPlayOne@users.noreply.github.com>
2025-12-02 10:13:29 +0100

    Add files via upload

---
+++ STYLE/script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    toggle.addEventListener("click", function() {
+      menu.classList.toggle("show");
+    });
+  });
+</script>
